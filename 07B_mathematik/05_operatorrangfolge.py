bb88ad1c15972e322d0af1a7e326a706ef01972d
adikaiser <adrian@kaiser.pt>
2021-01-17 18:06:55 +0100

    created solutions for chapter 7B

---
+++ 07B_mathematik/05_operatorrangfolge.py
@@ -0,0 +1,4 @@
+6 - 52 // 5 ** 2
+
+# Klammern zuerst, dann Exponenten, darauf folgen Division und Multiplikation, dann schließlich Addition und Subtraktion,
+# Die Ausgabe lautet: 4
---
+++ 07B_mathematik/07_a_feat_with_feet.py
@@ -0,0 +1,4 @@
+height = float(input())
+height_in_cm = height * 30.48
+
+print(height_in_cm)
