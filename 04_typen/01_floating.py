b6119030ffc4f2c4ff8b00ecd5d979dde319e1f5
adikaiser <adrian@kaiser.pt>
2021-01-17 09:22:52 +0100

    created solutions for chapter 4

---
+++ 04_typen/01_floating.py
@@ -0,0 +1,7 @@
+x = 1.5
+y = 2
+z = x * y
+print(x, y, z)
+print(type(x), type(y), type(z))
+
+# Die Ausgabe lautet: 1.5 2 3.0
---
+++ 04_typen/02_leckere_typumwandlung.py
@@ -0,0 +1,4 @@
+L = float(inputStr)
+A = L * L
+x = int(A / 100)
+print(x)
