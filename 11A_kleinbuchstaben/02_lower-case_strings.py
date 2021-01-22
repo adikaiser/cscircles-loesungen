55a14e45927195cf9cc8f254d2b25918ecaa1cd5
adikaiser <adrian@kaiser.pt>
2021-01-22 16:59:26 +0100

    created solutions for chapter 11A

---
+++ 11A_kleinbuchstaben/02_lower-case_strings.py
@@ -0,0 +1,12 @@
+def lowerChar(char):
+   if ord(char) >= ord('A') and ord(char) <= ord('Z'):
+      return chr(ord(char) + 32)
+   else:
+      return char
+   
+def lowerString(string):
+   result = ""
+   for i in range(len(string)):
+      result += lowerChar(string[i])
+   return result
+      
