0d2712c034c4a21dd8d7d148781e6fc45af4a7cb
Adrian Kaiser <adrian@kaiser.pt>
2021-01-27 14:07:00 +0100

    Update 06_the_name_game.py

--- 07A_strings/06_the_name_game.py
+++ 07A_strings/06_the_name_game.py
@@ -1,6 +1,6 @@
 my_string = input()
 
-print((my_string + chr(44) + chr(32)) * 2 + ("bo-b" + my_string[1:len(my_string)]))
+print((my_string + ", ") * 2 + ("bo-b" + my_string[1:len(my_string)]))
 print("banana-fana fo-f" + my_string[1:len(my_string)])
 print("fee-fi-mo-m"  + my_string[1:len(my_string)])
 print(my_string + "!")
