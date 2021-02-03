d6ba3f88a4ed8be1ccefc37b1d8a4e666c20b9ce
adikaiser <adrian@kaiser.pt>
2021-02-03 15:37:12 +0100

    added comments for additional understanding

--- 08_remix/01_python_adder.py
+++ 08_remix/01_python_adder.py
@@ -1,6 +1,6 @@
 S = input()
 for position in range(0, len(S)):
    if S[position] == '+':  # Zahlen zwischen '+' trennen
-      str1 = int(S[0:position])  # str1 in Integer umwandeln
-      str2 = int(S[(position+1):len(S)])  # str2 in Integer umwandeln
+      str1 = int(S[0:position])  # entspricht Substring vor + Symbol
+      str2 = int(S[(position+1):len(S)])  # entspricht Substring nach + Symbol
       print(str1 + str2)
