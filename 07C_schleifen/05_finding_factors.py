9e4333c98e146ded08abf86207b6cd3012450ff2
adikaiser <adrian@kaiser.pt>
2021-02-03 14:55:34 +0100

    added option to print actual counter value in loop

--- 07C_schleifen/05_finding_factors.py
+++ 07C_schleifen/05_finding_factors.py
@@ -1,6 +1,8 @@
 n = int(input())
 
 for outer in range(1 , (n + 1)):
+    # print("Aktueller Wert von outer: " + str(outer))
     for inner in range(1, (n+1)):
+        # print("Aktueller Wert von inner: " + str(inner))
         if outer * inner == n:
             print(str(outer)+" times "+str(inner)+" equals "+str(n))
